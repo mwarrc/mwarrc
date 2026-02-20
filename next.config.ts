136cb2b5b9a0126f11cc8a5c965a2eb30d6bc393
mwarrc <clintonmwariri@gmail.com>
2026-02-20 18:50:06 +0300

    fix: set basePath for repository mwarrc

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '',
+  basePath: '/mwarrc',
+  assetPrefix: '/mwarrc/',
   trailingSlash: true,
   images: {
     unoptimized: true,
